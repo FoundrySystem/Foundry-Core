32d8412e224ebef40c3f10700ef10b802cbbe4cf
Julian <julian.lipner07@gmail.com>
2025-11-12 23:01:58 +0100

    init

---
+++ src/main/java/at/foundry/core/FoundryPlugin.java
@@ -0,0 +1,33 @@
+package at.foundry.core;
+
+import jakarta.validation.constraints.NotNull;
+
+public abstract class FoundryPlugin {
+
+    public void start() {
+
+    }
+    public void stop() {
+
+    }
+    @NotNull
+    public abstract String getName();
+    @NotNull
+    public abstract String getVersion();
+
+    public String getDescription() {
+        return null;
+    }
+
+    public String getAuthor() {
+        return null;
+    }
+
+    public String getLogoLocation() {
+        return null;
+    }
+
+    @NotNull
+    public abstract String getFxmlEntryFile();
+
+}
